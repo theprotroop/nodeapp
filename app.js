05e5f45d3defc51a89e865931134304e289682ee
theprotroop <nvidiarahman@gmail.com>
2020-03-31 21:01:37 +0530

    my first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("<h2> Hellow world </h2>");
+});
+
+app.listen(3000,() => {
+    console.log("app is running on port 3000...");
+});
