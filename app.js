996cf1070945c638825780115b98bbd4a595bb99
theprotroop <nvidiarahman@gmail.com>
2020-04-07 20:54:12 +0530

    docker build and run ci/cd

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h2> Hellow world </h2>");
+    res.send("<h2> Hellow world from my node app </h2>");
 });
 
 app.listen(3000,() => {
